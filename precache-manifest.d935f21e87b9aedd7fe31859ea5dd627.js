2a415ef9ff979fa7ce98bac167d88e6f7b42c3ad
Jacob <57647839+jacobwies@users.noreply.github.com>
2020-06-11 11:42:29 -0700

    Updates

---
+++ precache-manifest.d935f21e87b9aedd7fe31859ea5dd627.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "253083faa8f62554f65977d8db9dfd4b",
+    "url": "/CommunityCurrency/index.html"
+  },
+  {
+    "revision": "61b304cdf49bcf543329",
+    "url": "/CommunityCurrency/static/css/main.e5035dff.chunk.css"
+  },
+  {
+    "revision": "4a91c3004970fee2442a",
+    "url": "/CommunityCurrency/static/js/2.4a6231cb.chunk.js"
+  },
+  {
+    "revision": "5bf520df75ebc8386445ba13594ae2df",
+    "url": "/CommunityCurrency/static/js/2.4a6231cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61b304cdf49bcf543329",
+    "url": "/CommunityCurrency/static/js/main.5795be62.chunk.js"
+  },
+  {
+    "revision": "eed8118b34260a12d04a",
+    "url": "/CommunityCurrency/static/js/runtime-main.1d7b3080.js"
+  },
+  {
+    "revision": "b36dc23775956fcceb3393aa5fdd36f8",
+    "url": "/CommunityCurrency/static/media/cc_logo.b36dc237.png"
+  }
+]);
